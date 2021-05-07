8282c765bc55d50eef2d04f817406cff51a043b0
Kpinnoes <khorapin.pingadpu@mail.kmutt.ac.th>
2021-05-08 01:56:54 +0700

    Update file blogs folder

--- src/blogs/BaseBrand.vue
+++ src/blogs/BaseBrand.vue
@@ -8,13 +8,13 @@
   </div>
   <div class="flex justify-center rounded-lg">
     <div 
-      class="flex rounded-lg flex-row  grid grid-flow-col grid-cols-3 grid-rows-4 md:grid md:grid-flow-col md:grid-cols-3 md:grid-rows-3 gap-4 "
+      class="flex rounded-lg flex-row  ml-8 grid grid-flow-col grid-cols-3 grid-rows-4 md:grid md:grid-flow-col md:grid-cols-3 md:grid-rows-3 gap-4 "
     > 
     <div class="hover:bg-pink-300 bg-white " v-for="brand in brandArray" :key="brand.brandId">
       <div
         class=" flex bg-white rounded-lg :hoverbg-blue-100  md:h-80 md:w-80 w-48 h-48 shadow-lg rounded-lg"
       >
-        <img class="md:w-72 w-48 h-48 " :src="brand.brandImage" alt="brandImage" />
+        <img class="md:w-84 w-48 h-48 " :src="brand.brandImage" alt="brandImage" />
         <p class="text-lg  font-bold text-blue-900"><br>{{ brand.brandName }}</p>
       </div>
     </div>
