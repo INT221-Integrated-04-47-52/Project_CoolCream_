8d0b5b1ad51a11c59d5d465a04ff5fc9a5dc9789
Kpinnoes <khorapin.pingadpu@mail.kmutt.ac.th>
2021-05-07 21:42:04 +0700

    Updated files

--- src/main.js
+++ src/main.js
@@ -2,17 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 
+
 import router from './router'
 import AllProduct from './components/AllProduct'
 import RegisterForm from './components/RegisterForm'
-import HomeView from './components/HomeView'
-import BlogTopping from  './components/BlogTopping'
-import FirstHome from  './components/FirstHome'
+import HomeView from './UI/HomeView'
+import BaseTopping from  './blogs/Baseopping'
+import FirstHome from  './UI/FirstHome'
+import BaseBrand from  './blogs/BaseBrand'
 
 const app = createApp(App)
 app.component('register-form', RegisterForm)
 app.component('home-view', HomeView)
 app.component('all-product', AllProduct)
-app.component('blog-topping', BlogTopping)
+app.component('base-topping', BaseTopping)
 app.component('first-home', FirstHome)
+app.component('base-brand', BaseBrand)
 app.use(router).mount('#app')
