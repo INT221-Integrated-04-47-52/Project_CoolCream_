68e03cd472594f32460f800f885f7ac452f5af45
Omega 3 <60165721+Nnook@users.noreply.github.com>
2021-05-10 04:32:23 +0700

    Update Popup.vue

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -259,21 +259,21 @@ export default {
       }
     },
     async fetchTopping() {
-      const res = await fetch("http://localhost:6001/topping");
+      const res = await fetch(`${process.env.VUE_APP_ROOT_API}topping`);
       const data = await res.json();
       return data;
     },
     async fetchSize() {
-      const res = await fetch("http://localhost:6001/size");
+      const res = await fetch(`${process.env.VUE_APP_ROOT_API}size`);
       const data = await res.json();
       return data;
     },
     async fetchBrand() {
-      const res = await fetch("http://localhost:6001/brand");
+      const res = await fetch(`${process.env.VUE_APP_ROOT_API}brand`);
       const data = await res.json();
       return data;
     },async fetchIcecreams() {
-      const res = await fetch("http://localhost:6001/icecream"+"/"+ this.icecream.icecreamId);
+      const res = await fetch(`${process.env.VUE_APP_ROOT_API}icecream`+"/"+ this.icecream.icecreamId);
       const data = await res.json();
       return data;
     },
